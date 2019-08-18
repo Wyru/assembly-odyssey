47cb32cc935c66677ff54cb5db9280c503a0b5a1
Will Saymon <wsaymonoficial@gmail.com>
2019-08-18 11:14:28 -0300

    Add Puzzle Spawn, better configs and level system

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public Transform piecesContainer;
+    public List<Transform> spawnPoints;
+    public List<GameObject> puzzles;
+    public List<PieceJoint> sockets;
+    int currentPuzzle;
+    bool settingPuzzle;
+    bool gameIsEnd;
+
+    private void Start()
+    {
+        settingPuzzle = true;
+        currentPuzzle = 0;
+        gameIsEnd = false;
+        StartCoroutine("SpawnPuzzle");
+    }
+
+    private void Update()
+    {
+
+        if (gameIsEnd)
+            return;
+
+        if (!settingPuzzle){
+        
+            if (sockets.TrueForAll((PieceJoint a) =>
+            {
+                return a.joinSuccess;
+            }))
+            {
+                settingPuzzle = true;
+                StartCoroutine("SpawnPuzzle");
+                currentPuzzle++;
+            }
+        }
+    }
+
+    IEnumerator SpawnPuzzle()
+    {
+        PieceBehavior[] pieces = piecesContainer.GetComponentsInChildren<PieceBehavior>();
+
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            Destroy(pieces[i].gameObject);
+        }
+
+        yield return new WaitForSeconds(.1f);
+
+        if (currentPuzzle >= puzzles.Count)
+        {
+            Debug.Log("Obrigado por ter jogar ^^");
+            gameIsEnd = true;
+        }
+
+        if (!gameIsEnd)
+        {
+            PieceBehavior[] pb = puzzles[currentPuzzle].GetComponentsInChildren<PieceBehavior>();
+
+            for (int i = 0; i < pb.Length; i++)
+            {
+                Vector3 position = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
+                Quaternion rotation = new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+                Instantiate(pb[i], position, Quaternion.identity, piecesContainer);
+                yield return new WaitForSeconds(.1f);
+            }
+
+            sockets = new List<PieceJoint>(piecesContainer.GetComponentsInChildren<PieceJoint>());
+
+            sockets = sockets.FindAll((PieceJoint p) =>
+            {
+                return p.isCenterJoint;
+            });
+
+            settingPuzzle = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PieceJoint.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class PieceJoint : MonoBehaviour
+{
+    [BoxGroup("References")]public ParticleSystem successParticles;
+    [BoxGroup("Settings")]public bool isSocket;
+    [BoxGroup("Settings"), ShowIf("isSocket")]public bool isCenterJoint;
+    [BoxGroup("Settings"), ShowIf("isSocket")]public PieceJoint RightJoint;
+    [BoxGroup("Settings"), ShowIf("isCenterJoint")]public List<PieceJoint> corners;
+    public bool joinSuccess;
+    [HideInInspector]public PieceJoint currentJoint;
+
+    private void Update()
+    {
+        if (isSocket)
+        {
+            if (isCenterJoint)
+            {
+                if (!joinSuccess && corners.TrueForAll((PieceJoint a) =>
+                {
+                    return a.joinSuccess;
+                }))
+                {
+                    successParticles?.Play();
+                    joinSuccess = true;
+                    currentJoint.transform.parent.SetParent(transform.parent);
+                    currentJoint.GetComponentInParent<PieceBehavior>().parent = GetComponentInParent<PieceBehavior>();
+                    Debug.Log("success");
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isSocket)
+        {
+            currentJoint = other.GetComponent<PieceJoint>();
+
+            if (!isCenterJoint)
+            {
+                if (RightJoint != null)
+                {
+                    if (currentJoint == RightJoint)
+                    {
+                        if (!isCenterJoint)
+                        {
+                            joinSuccess = true;
+                        }
+                    }
+                }
+                else
+                {
+
+                    joinSuccess = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isSocket)
+        {
+            if (!isCenterJoint)
+            {
+                if (RightJoint != null)
+                {
+                    currentJoint = other.GetComponent<PieceJoint>();
+                    if (currentJoint == RightJoint)
+                    {
+                        joinSuccess = false;
+                    }
+                }
+                else
+                {
+                    joinSuccess = false;
+                }
+            }
+        }
+    }
+}
